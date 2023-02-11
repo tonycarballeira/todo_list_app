75c6a297ad2e5165f81999b7533757b2e7dde293
tonycarballeira <tonycarballeira@gmail.com>
2023-02-11 13:52:51 -0500

    added functions to handle inputs and add tasks to list

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const newTodoList = [...todoList, newTask];
+    setTodoList(newTodoList);
+  };
+
   return (
     <div className="App">
-     
+     <div className='addTask'>
+        <input onChange={handleChange}/>
+        <button onClick={addTask}> Add Task </button>
+     </div>
+     <div className='list'>
+      {todoList.map((task) => {
+        return <h1>{task}</h1>;
+      })}
+     </div>
     </div>
   );
 }
